6a39c6dc5e40cea608c5904f013311c996def41c
rythm07 <joaonunocorvo@gmail.com>
2022-02-22 23:22:05 +0000

    - - BehaviorTree.CPP -> tutorial_03_generic_ports.md

---
+++ src/BT_example/include/goal_interface.hpp
@@ -0,0 +1,56 @@
+#ifndef __CalculateGoal_H_
+#define __CalculateGoal_H_
+
+#include "behaviortree_cpp_v3/action_node.h"
+#include "convert_from_string.hpp"
+
+using namespace BT;
+
+class CalculateGoal : public SyncActionNode
+{
+public:
+  CalculateGoal(const std::string &name, const NodeConfiguration &config) : SyncActionNode(name, config)
+  {
+  }
+
+  static PortsList providedPorts()
+  {
+    return {OutputPort<Position2D>("goal")};
+  }
+
+  NodeStatus tick() override
+  {
+    Position2D mygoal = {1.1, 2.3};
+    setOutput<Position2D>("goal", mygoal);
+    return NodeStatus::SUCCESS;
+  }
+};
+
+class PrintTarget : public SyncActionNode
+{
+public:
+  PrintTarget(const std::string &name, const NodeConfiguration &config) : SyncActionNode(name, config)
+  {
+  }
+
+  static PortsList providedPorts()
+  {
+    // Optionally, a port can have a human readable description
+    const char *description = "Simply print the goal on console...";
+    return {InputPort<Position2D>("target", description)};
+  }
+
+  NodeStatus tick() override
+  {
+    auto res = getInput<Position2D>("target");
+    if (!res)
+    {
+      throw RuntimeError("error reading port [target]:", res.error());
+    }
+    Position2D target = res.value();
+    printf("Target positions: [ %.1f, %.1f ]\n", target.x, target.y);
+    return NodeStatus::SUCCESS;
+  }
+};
+
+#endif
